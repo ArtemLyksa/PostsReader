1895bce5a168062d8560dd0df9e76a7cb46a2b3a
Artem <artemlyksa@artems-macbook-pro.local>
2019-02-28 20:28:01 +0200

    Initial Commit

---
+++ PostsReader/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  PostsReader
+//
+//  Created by Artem Lyksa on 2/28/19.
+//  Copyright © 2019 lyksa. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
